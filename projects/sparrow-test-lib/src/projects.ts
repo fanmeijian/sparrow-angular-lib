739c648b4f98eab05b43a66ae6ea3dae3c9aadd1
Sword <fanmeijian@gmail.com>
2024-05-12 17:41:29 +0800

    remove unused

--- projects/sparrow-test-lib/src/projects.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of sparrow-test-lib
- */
-
-export * from './lib/sparrow-test-lib.service';
-export * from './lib/sparrow-test-lib.component';
-export * from './lib/sparrow-test-lib.module';
